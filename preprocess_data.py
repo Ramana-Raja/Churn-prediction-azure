1a6d0ae374ddaac74913af9955f92785aba3144f
Ramana-Raja <ramanarajakesavaraja@gmail.com>
2025-06-18 19:49:31 +0400

    made changes to preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from pandas import read_csv
+import os
+from io import BytesIO, StringIO
+from azure.storage.blob import BlobServiceClient
 
 df = pd.read_csv("dataset.csv")
 
@@ -32,11 +34,28 @@ for col in yes_no_cols:
     df[col] = np.where(df[col] == 'Yes', 1, 0)
 
 
+def plot_corr():
+    corr = df.corr()
+    plt.figure(figsize=(10, 8))
+    plt.imshow(corr, cmap='coolwarm', interpolation='none')
+    plt.colorbar()
+    plt.xticks(range(len(corr)), corr.columns, rotation=90)
+    plt.yticks(range(len(corr)), corr.columns)
+    plt.show()
+df_no_index = df.reset_index(drop=True)
 
-corr = df.corr()
-plt.figure(figsize=(10, 8))
-plt.imshow(corr, cmap='coolwarm', interpolation='none')
-plt.colorbar()
-plt.xticks(range(len(corr)), corr.columns, rotation=90)
-plt.yticks(range(len(corr)), corr.columns)
-plt.show()
+parquet_buffer = BytesIO()
+df_no_index .to_parquet(parquet_buffer)
+parquet_buffer.seek(0)
+
+storage_account_name = os.getenv("AZURE_STORAGE_ACCOUNT")
+storage_account_key = os.getenv("AZURE_STORAGE_KEY")
+
+connection_string = f"DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={storage_account_key};EndpointSuffix=core.windows.net"
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+container_name = "mlopsdata"
+blob_name = "processed_data.parquet"
+
+blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+blob_client.upload_blob(parquet_buffer.getvalue(), overwrite=True)
