0e726971e988fbbdef1880d81fd13e8200eb73a6
Ramana-Raja <ramanarajakesavaraja@gmail.com>
2025-06-23 01:27:39 +0400

    made changes for azure

--- kubernetes/codes/monitoring/monitoring.py
+++ kubernetes/codes/monitoring/monitoring.py
@@ -1,15 +1,16 @@
 import time
-
 import pandas as pd
-
-from evidently import Report
-from evidently import DataDefinition
-from evidently import Dataset
+from evidently import Report, DataDefinition, Dataset
 from evidently.metrics import ValueDrift, DriftedColumnsCount, MissingValueCount
 import joblib
 import os
 from azure.storage.blob import BlobServiceClient
 from io import BytesIO
+from prometheus_client import Gauge, start_http_server
+
+drift_value = Gauge('data_drift_value', 'Drift value of Churn')
+drifted_columns_count = Gauge('drifted_columns_count', 'Number of drifted columns')
+missing_value_count = Gauge('missing_value_count', 'Missing value count in Churn')
 
 def monitor():
     storage_account_name = os.getenv("AZURE_STORAGE_ACCOUNT")
@@ -23,34 +24,20 @@ def monitor():
     blob_name_test = "test.parquet"
     blob_model = "model.pkl"
 
-
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name_train)
-
-    download_stream = BytesIO()
-    download_stream.write(blob_client.download_blob().readall())
-    download_stream.seek(0)
-
-    df_train = pd.read_parquet(download_stream)
+    stream = BytesIO(blob_client.download_blob().readall())
+    df_train = pd.read_parquet(stream)
 
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name_test)
-
-    download_stream = BytesIO()
-    download_stream.write(blob_client.download_blob().readall())
-    download_stream.seek(0)
-    df_test = pd.read_parquet(download_stream)
+    stream = BytesIO(blob_client.download_blob().readall())
+    df_test = pd.read_parquet(stream)
 
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_model)
-
     model_bytes = blob_client.download_blob().readall()
-    buffer = BytesIO(model_bytes)
+    model = joblib.load(BytesIO(model_bytes))
 
-    model = joblib.load(buffer)
-
-
-    df_test = df_test.drop('Churn',axis=1)
-
+    df_test = df_test.drop('Churn', axis=1)
     df_test['Churn'] = model.predict(df_test)
-
 
     data_definition = DataDefinition(
         numerical_columns=list(df_train.columns),
@@ -60,27 +47,24 @@ def monitor():
     current_dataset = Dataset.from_pandas(df_test, data_definition=data_definition)
     reference_dataset = Dataset.from_pandas(df_train, data_definition=data_definition)
 
-    report = Report(metrics = [
+    report = Report(metrics=[
         ValueDrift(column='Churn'),
         DriftedColumnsCount(),
         MissingValueCount(column='Churn'),
     ])
 
     run = report.run(reference_data=reference_dataset, current_data=current_dataset)
-
     result = run.dict()
-    print(result['metrics'][0]['value'])
-    print(result['metrics'][1]['value']['count'])
-    print(result['metrics'][2]['value']['share'])
+
+    drift_value.set(result['metrics'][0]['value'])
+    drifted_columns_count.set(result['metrics'][1]['value']['count'])
+    missing_value_count.set(result['metrics'][2]['value']['count'])
 
 if __name__ == "__main__":
-    import logging
-    logging.basicConfig(
-        filename='/var/log/myapp.log',
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
-    )
+
+    start_http_server(8000)
 
     while True:
-        logging.info("Python app started")
-        time.sleep(5)
+        monitor()
+        print("Drift check completed. Next check in 5 hours.")
+        time.sleep(5 * 60 )
