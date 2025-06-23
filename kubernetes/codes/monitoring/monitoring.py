0991342879fa6af00347c5ac4c62056648bc8cb7
Ramana-Raja <ramanarajakesavaraja@gmail.com>
2025-06-24 02:00:53 +0400

    UPDATED SOME STUFF

--- kubernetes/codes/monitoring/monitoring.py
+++ kubernetes/codes/monitoring/monitoring.py
@@ -8,63 +8,107 @@ from azure.storage.blob import BlobServiceClient
 from io import BytesIO
 from prometheus_client import Gauge, start_http_server
 
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import ReadableSpan, SpanProcessor
+
+
+
 drift_value = Gauge('data_drift_value', 'Drift value of Churn')
 drifted_columns_count = Gauge('drifted_columns_count', 'Number of drifted columns')
 missing_value_count = Gauge('missing_value_count', 'Missing value count in Churn')
 
+resource = Resource(attributes={
+    "service.name": "python-monitoring-app"
+})
+
+provider = TracerProvider(resource=resource)
+trace.set_tracer_provider(provider)
+
+otlp_exporter = OTLPSpanExporter(
+    endpoint="http://tempo.monitoring.svc.cluster.local:4317",
+    insecure=True
+)
+
+span_processor = BatchSpanProcessor(otlp_exporter)
+provider.add_span_processor(span_processor)
+
+
+tracer = trace.get_tracer(__name__)
+
 def monitor():
-    storage_account_name = os.getenv("AZURE_STORAGE_ACCOUNT")
-    storage_account_key = os.getenv("AZURE_STORAGE_KEY")
+    with tracer.start_as_current_span("monitor-function") as span:
+        storage_account_name = os.getenv("AZURE_STORAGE_ACCOUNT")
+        storage_account_key = os.getenv("AZURE_STORAGE_KEY")
 
-    connection_string = f"DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={storage_account_key};EndpointSuffix=core.windows.net"
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        connection_string = f"DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={storage_account_key};EndpointSuffix=core.windows.net"
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
-    container_name = "mlopsdata"
-    blob_name_train = "train.parquet"
-    blob_name_test = "test.parquet"
-    blob_model = "model.pkl"
+        container_name = "mlopsdata"
+        blob_name_train = "train.parquet"
+        blob_name_test = "test.parquet"
+        blob_model = "model.pkl"
 
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name_train)
-    stream = BytesIO(blob_client.download_blob().readall())
-    df_train = pd.read_parquet(stream)
+        with tracer.start_as_current_span("download-train-data"):
+            blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name_train)
+            stream = BytesIO(blob_client.download_blob().readall())
+            df_train = pd.read_parquet(stream)
+            logging.info("Downloaded df_train")
 
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name_test)
-    stream = BytesIO(blob_client.download_blob().readall())
-    df_test = pd.read_parquet(stream)
+        with tracer.start_as_current_span("download-test-data"):
+            blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name_test)
+            stream = BytesIO(blob_client.download_blob().readall())
+            df_test = pd.read_parquet(stream)
+            logging.info("Downloaded df_test")
 
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_model)
-    model_bytes = blob_client.download_blob().readall()
-    model = joblib.load(BytesIO(model_bytes))
+        with tracer.start_as_current_span("download-model"):
+            blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_model)
+            model_bytes = blob_client.download_blob().readall()
+            model = joblib.load(BytesIO(model_bytes))
+            logging.info("Downloaded model")
 
-    df_test = df_test.drop('Churn', axis=1)
-    df_test['Churn'] = model.predict(df_test)
+        with tracer.start_as_current_span("predict-and-generate-report"):
+            df_test = df_test.drop('Churn', axis=1)
+            df_test['Churn'] = model.predict(df_test)
 
-    data_definition = DataDefinition(
-        numerical_columns=list(df_train.columns),
-        categorical_columns=[],
-    )
+            data_definition = DataDefinition(
+                numerical_columns=list(df_train.columns),
+                categorical_columns=[],
+            )
 
-    current_dataset = Dataset.from_pandas(df_test, data_definition=data_definition)
-    reference_dataset = Dataset.from_pandas(df_train, data_definition=data_definition)
+            current_dataset = Dataset.from_pandas(df_test, data_definition=data_definition)
+            reference_dataset = Dataset.from_pandas(df_train, data_definition=data_definition)
 
-    report = Report(metrics=[
-        ValueDrift(column='Churn'),
-        DriftedColumnsCount(),
-        MissingValueCount(column='Churn'),
-    ])
+            report = Report(metrics=[
+                ValueDrift(column='Churn'),
+                DriftedColumnsCount(),
+                MissingValueCount(column='Churn'),
+            ])
 
-    run = report.run(reference_data=reference_dataset, current_data=current_dataset)
-    result = run.dict()
+            run = report.run(reference_data=reference_dataset, current_data=current_dataset)
+            result = run.dict()
 
-    drift_value.set(result['metrics'][0]['value'])
-    drifted_columns_count.set(result['metrics'][1]['value']['count'])
-    missing_value_count.set(result['metrics'][2]['value']['count'])
+            drift_value.set(result['metrics'][0]['value'])
+            drifted_columns_count.set(result['metrics'][1]['value']['count'])
+            missing_value_count.set(result['metrics'][2]['value']['count'])
 
 if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(
+        filename='/var/log/myapp/myapp.log',
+        filemode='a',
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
 
     start_http_server(8000)
 
     while True:
+        logging.info("Monitor started")
         monitor()
-        print("Drift check completed. Next check in 5 hours.")
         time.sleep(5 * 60 )
+        logging.info("Monitor finished, next in 5 minutes")
